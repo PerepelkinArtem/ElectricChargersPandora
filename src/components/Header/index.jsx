e87701b05657c478297ac0a044c36d41eebf3a1d
Perepelkin Artem <perepelkin-klg@yandex.ru>
2022-10-02 20:35:16 +0300

    Station button in blueline

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -9,12 +9,15 @@ const Header = () => {
         <header>
             <div className="blueLine">
                 <a href="tel:+78007001718" >8 (800) 700-17-18 (горячая линия техподдержки)</a>
-                <Link to='/login'>
-                    <div className="userArea">
-                        <span>Perepelkin Artem</span>
+                <div className="userArea">
+                    <span>Perepelkin Artem</span>
+                    <Link to='/login'>
                         <img with={25} height={25} className="userLogo" src="/img/user.svg" alt="UserLogo" />
-                    </div>
-                </Link>
+                    </Link>
+                    <Link to='/'>
+                        <img with={25} height={25} className="stationLogo" src="/img/charging-station-white.png" alt="StationLogo" />
+                    </Link>
+                </div>
             </div>
             <Link to='/'>
                 <div className="headerLogo">

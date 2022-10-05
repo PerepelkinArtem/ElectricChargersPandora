9dfb3050e1cbb7e9e0ad420187233b0415bedc97
Perepelkin Artem <perepelkin-klg@yandex.ru>
2022-10-05 10:35:00 +0300

    working code in Login.jsx for get and post

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,21 +1,84 @@
 import React, {useState} from 'react'
+import axios from 'axios'
 
 import './Login.css'
 
 const Login = () => {
 
+// const baseUrl = 'http://web1.pandora.develop:5003/api/login';
+
+// axios.post('http://web1.pandora.develop:5003/api/login', {
+//     firstName: 'a.perepelkin',
+//     lastName: '3f86ou'
+//   })
+//   .then(function (response) {
+//     console.log(response);
+//   })
+//   .catch(function (error) {
+//     console.log(error);
+//   });
+
+
+
+// // working code for post!!!!!!!!!!!!!!!!!!!!
+const body = {
+    "login": "a.perepelkin",
+    "password": "3f86ou"
+  }
+
+axios({
+    method: 'post',
+    url: 'http://web1.pandora.develop:5003/api/login',
+    data: body
+})
+.then(function (response) {
+    console.log(response);
+})
+.catch(function (error) {
+    console.log(error);
+});
+
+
+// // working code for get
+//   axios.get('https://622072c8ce99a7de1959cf52.mockapi.io/items')
+//   .then(function (response) {
+//     console.log(response);
+//   })
+//   .catch(function (error) {
+//     console.log(error);
+//   });
+
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
 
+    // (async () => {
+    //     const response = await fetch('http://web1.pandora.develop:5003/api/login', {
+    //         method: 'POST',
+    //         headers: {
+    //             authorization: '1qazxsw2',
+    //             'content-type': 'application/json'
+    //         },
+    //         body: JSON.stringify({ name: 'Андрей' })
+    //     });
+
+    //     if (!response.ok) {
+    //         console.error('Запрос не удался');
+    //         return;
+    //     }
+
+    //     const data = await response.json();
+    //     console.log(data);
+    // })();
+
     return (
-        <div class="login-box">
-            <h2>Авторизация</h2>
+        <div className="login-box">
+            <h2>Аутентификация</h2>
             <form>
-                <div class="user-box">
+                <div className="user-box">
                     <input type="text" name="" required="" onChange={event => setPassword(event.target.value)} />
                         <label>Имя пользователя</label>
                 </div>
-                <div class="user-box">
+                <div className="user-box">
                     <input type="password" name="" required="" onChange={event => setUsername(event.target.value)} />
                         <label>Пароль</label>
                 </div>

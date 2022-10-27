004da7f137508e1353a4ebc7e1eccf0d20b78254
PerepelkinArtem <perepelkin-klg@yandex.ru>
2022-10-27 23:10:20 +0300

    Login form modification

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -1,40 +1,73 @@
-import React, { useState } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 
 import './Form.css'
 
 const Form = (title, handleClick) => {
-    const [email, setEmail] = useState('');
-    const [pass, setPass] = useState('');
+
+    const userRef = useRef()
+    const errRef = useRef()
+
+    const [user, setUser] = useState('')
+    const [pwd, setPwd] = useState('')
+    const [errMsg, setErrMsg] = useState('')
+    const [success, setSuccess] = useState(false)
+
+    useEffect(() => {
+        userRef.current.focus();
+    }, [])
+
+    useEffect(() => {
+        setErrMsg('');
+    }, [user, pwd])
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        console.log(user, pwd);
+        setUser('');
+        setPwd('');
+        setSuccess(true);
+    }
 
     return (
-        <div className="login-box">
-            <h2>Аутентификация</h2>
-            <form>
-                <div className="user-box">
-                    <input
-                        type="email"
-                        value={email}
-                        placeholder="email"
-                        onChange={(e) => setEmail(e.target.value)} />
-                    <label>E-mail</label>
+        <>
+            {success ? (
+                <section>
+                    <h1>Вход выполнен!</h1>
+                    <br />
+                </section>
+            ) : (
+                <div className="login-box">
+                    <h2>Войти</h2>
+                    <form onSubmit={handleSubmit}>
+                        <div className="user-box">
+                            <input
+                                type="text"
+                                id="username"
+                                ref={userRef}
+                                autoComplete="off"
+                                onChange={(e) => setUser(e.target.value)}
+                                value={user}
+                                required
+                            />
+                            <label>Имя пользователя</label>
+                        </div>
+                        <div className="user-box">
+                            <input
+                                type="password"
+                                id="password"
+                                onChange={e => setPwd(e.target.value)}
+                                value={pwd}
+                                required
+                            />
+                            <label>Пароль</label>
+                        </div>
+                        <button className="enterButtonForm">
+                            Войти
+                        </button>
+                    </form>
                 </div>
-                <div className="user-box">
-                    <input
-                        type="password"
-                        value={pass}
-                        placeholder="password"
-                        onChange={e => setPass(e.target.value)} />
-                    <label>Пароль</label>
-                </div>
-                <button
-                    className="enterButtonForm"
-                    onClick={() => handleClick(email, pass)}
-                >
-                    {/* {title} */}
-                    Войти
-                </button>
-            </form>
-        </div>
+            )}
+        </>
     )
 }
 export default Form
